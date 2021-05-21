0e3372a423d0f88bece84c6c3742d23c576aafc6
William N. French <f0031m3@babylon5.thayer.dartmouth.edu>
2021-05-21 10:13:53 -0400

    pulled in visbility

---
+++ common/network.c
@@ -0,0 +1,86 @@
+/*
+ * network.c - CS50 'network' module
+ *
+ * see network.c for further information
+ *
+ * Alan Moss, April 2021
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include "network.h"
+#include "mem.h"
+
+/**************** file-local global variables ****************/
+/* none */
+
+/**************** local types ****************/
+
+
+/**************** global types ****************/
+
+
+/**************** global functions ****************/
+/* that is, visible outside this file */
+/* see hashtable.h for comments about exported functions */
+
+/**************** local functions ****************/
+/* not visible outside this file */
+
+
+/**************** startNetworkServer() ****************/
+/* see network.h for description */
+// handle message, they will enver get the same message.
+void
+startNetworkServer(gameInfo_t* gameInfo);
+{
+
+}
+
+/**************** startNetworkClient() ****************/
+/* see network.h for description */
+// handle message, they will enver get the same message.
+void
+startNetworkClient(char* serverHost, char* port)
+{
+  
+}
+
+/**************** tokenizeMessage() ****************/
+/* see network.h for description */
+// handle message, they will enver get the same message.
+char**
+tokenizeMessage(const char* message)
+{
+  
+}
+
+/**************** handleMessage() ****************/
+/* see network.h for description */
+// handle message, they will enver get the same message.
+bool
+handleMessage(void* arg, const addr_t from, const char* message)
+{
+  
+}
+
+/**************** handleTimeout() ****************/
+/* see network.h for description */
+// handle message, they will enver get the same message.
+bool
+handleTimeout(void* arg)
+{
+  
+}
+
+/**************** handleInput() ****************/
+/* see network.h for description */
+// handle message, they will enver get the same message.
+bool
+handleInput(void* arg);
+{
+  
+}
+
