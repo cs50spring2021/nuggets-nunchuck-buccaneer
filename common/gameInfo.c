65ea5a138c26b822b10e805bffcb30d98250671b
William N. French <f0031m3@babylon5.thayer.dartmouth.edu>
2021-05-30 14:41:17 -0400

    client.c parse args tweak gameinfo tweak

--- common/gameInfo.c
+++ common/gameInfo.c
@@ -300,7 +300,6 @@ gameInfo_getPlayerFromID(gameInfo_t* info, int playerID)
     }
 
     // search the players array and find the player with the given playerID
-    int i = 0;
     for(int i = 0; i < info->maxPlayers; i++){
         if((info->players)[i] != NULL){
             if (playerID == (info->players)[i]->playerID) {
